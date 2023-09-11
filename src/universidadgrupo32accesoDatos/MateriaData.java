427c30c9bc7b1c4283a671cc94f845c89ee36260
Johana-Mansilla <141686809+Johana-Mansilla@users.noreply.github.com>
2023-09-11 20:51:19 -0300

    a

--- src/universidadgrupo32accesoDatos/MateriaData.java
+++ src/universidadgrupo32accesoDatos/MateriaData.java
@@ -1,7 +1,38 @@
 
 package universidadgrupo32accesoDatos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.mariadb.jdbc.Statement;
+import universidadgrupo32entidades.Materia;
+
 
 public class MateriaData {
+    private Connection con=null;
     
+    public MateriaData(){
+            con=Conexion.getConexion();
+            
 }
+    public void guardarMateria(Materia materia){
+    String sql="INSERT INTO materia(nombre,anio, estado)"
+            + "VALUES(?,?,?)";
+        try {
+            PreparedStatement ps=con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, materia.getNombre());
+            ps.setInt(2, materia.getAnio());
+            ps.setBoolean(3, materia.isEstado());
+            ps.executeUpdate();
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla materia");        }
+          
+    }
+}    
+    
+    
+
