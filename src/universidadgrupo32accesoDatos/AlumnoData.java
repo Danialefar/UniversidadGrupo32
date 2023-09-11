e8614b01d682da23d4314685bb0316f3fe01eaf2
Danialefar <enc_danielfarias@hotmail.com.ar>
2023-09-11 18:55:23 -0300

    11-09

--- src/universidadgrupo32accesoDatos/AlumnoData.java
+++ src/universidadgrupo32accesoDatos/AlumnoData.java
@@ -1,7 +1,43 @@
-
 package universidadgrupo32accesoDatos;
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+import universidadgrupo32entidades.Alumno;
 
 public class AlumnoData {
-    
+
+    public AlumnoData() {
+
+        con = Conexion.getConexion();
+    }
+
+    private Connection con = null;
+
+    public void guardarAlumno(Alumno alumno) throws SQLException {
+
+        String sql= "INSERT INTO alumno (dni, apellido, nombre, fechaNac, estado)"
+                + "VALUES (?,?,?,?,?)";
+        
+        try 
+            (PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2,alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNac()));
+            ps.setBoolean(5, alumno.isActivo());
+            
+            ps.executeUpdate();
+            
+            ResultSet rs= ps.getGeneratedKeys();
+            
+            if(rs.next()){
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                JOptionPane.showMessageDialog(null, "ALUMNO AÑADIDO CON EXITO");
+            }
+            ps.close();
+        }catch(SQLException ex){
+        JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA ALUMNO" + ex.getMessage());
+        }
+    }
+
 }
