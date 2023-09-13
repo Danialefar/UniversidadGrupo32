86421150618c5baae9ea055a0d6e630f72e9a284
johnbajer <79484631+johnbajer@users.noreply.github.com>
2023-09-13 18:17:08 -0300

    nuevo

--- src/universidadgrupo32accesoDatos/InscripcionData.java
+++ src/universidadgrupo32accesoDatos/InscripcionData.java
@@ -44,6 +44,32 @@ public class InscripcionData {
          
          
      }
+     public Inscripcion buscarInscripcion(int id) {
+    
+    String sql = "SELECT idInscripcion, nota , idAlumno, idMateroa FROM inscripcion WHERE idIscripcion = ?";
+    Inscripcion inscripcion = null;
+    try {
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+                inscripcion=new Inscripcion();
+                inscripcion.setNota(rs.getInt("nota"));
+                inscripcion.setAlumno.(rs.getInt("idAlumno"));
+                inscripcion.setMateria(rs.getInt("idMateria"));
+                inscripcion.setIdInscripcion(rs.getInt("idInscripcion"));
+
+         } else {
+              JOptionPane.showMessageDialog(null, "No existe la Inscripcion");
+        }
+        ps.close();
+    } catch (SQLException ex) {
+    JOptionPane.showMessageDialog(null, "Error al acceder a la tabla inscripcion ");
+    }    
+        return inscripcion;
+    
+    }    
              
      
 }
